a0f7e990ce4b4bfe02b049faa8dc239b9ea197d2
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2024-05-11 19:49:02 +0200

    safety first

--- server/preprocessing.py
+++ server/preprocessing.py
@@ -1,13 +1,11 @@
 
+import dateutil.parser
 import pm4py
 import os
 import pandas as pd
 import pydoc_markdown
-from ERPP_RMTPP import * 
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 import numpy as np
-
+import dateutil
 
 
 
@@ -36,19 +34,28 @@ class Preprocessing:
         """
         self.event_log = pm4py.read.read_xes(path)
 
-    def import_event_log_csv(self, path, case_id, activity_key, timestamp_key): 
+
+
+
+    def import_event_log_csv(self, path, case_id, activity_key, timestamp_key, sep): 
         """
         this is an adapter for format_dataframe such that 
         the event data can be properly used by the rnn model. 
 
         :param path: path to the event log
         :param case_id: case id column name
+        :param activity_key: activity column name
+        :param timestamp_key: timestamp column name
+        :param sep: separator
         """
-        dataframe= pd.read_csv(path)
+        dataframe= pd.read_csv(path, sep=sep)
         # this line transforms the event log in the required input 
         # for the RNN: groups the data by id and sorts the entries
         # accorting to time. 
         dataframe = dataframe.groupby(case_id).apply(lambda x: x.sort_values(timestamp_key)).reset_index(drop=True)
+        self.event_df = dataframe.copy()
+        self.event_df[timestamp_key] = pd.to_datetime(self.event_df[timestamp_key])
+
         dataframe = pm4py.format_dataframe(dataframe, 
                                            case_id=case_id,
                                              activity_key=activity_key,
@@ -56,8 +63,10 @@ class Preprocessing:
         self.case_id_key =  case_id
         self.case_activity_key = activity_key 
         self.case_timestamp_key = timestamp_key 
+
         self.event_log = pm4py.convert_to_event_log(dataframe)
-    """
+        #dataframe.to_csv("sorted_event_log.csv")
+        """
         TODO: 
         - group the data such that: 
             - all processes with = case id are contiguous
@@ -65,17 +74,21 @@ class Preprocessing:
             case id are sorted timewise. 
         - VERY IMPORTANT requirement for the 
         algorithm's implementation!!
-    """
+        """
 
     def split_train_test(self, train_percentage):
         """
         :param train_percentage: what percentage should be used for training
         :returns: two event logs, one for training and one for training  TODO
         """
-        train, test = pm4py.ml.split_train_test(self.event_log, train_percentage)
+        
+        train, test = pm4py.ml.split_train_test(self.event_df, train_percentage, self.case_id_key)
+        #: the author uses floats for representing time
+        train[self.case_timestamp_key] = train[self.case_timestamp_key].map(lambda x: x.timestamp())
+        test[self.case_timestamp_key] = test[self.case_timestamp_key].map(lambda x: x.timestamp())
         return train, test
 
-    
+
 
     def convert_csv_to_pandas(self):
         pass
