8d03d8cbd9848fa534455a5457e0e803ac9587c3
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2024-05-12 22:37:17 +0200

    sunday

--- server/main.py
+++ server/main.py
@@ -9,22 +9,15 @@ if __name__=="__main__":
 
 
     preprocessor = Preprocessing()
-
-    preprocessor.import_event_log_xes("../data/Hospital_log.xes")
-    print(preprocessor.event_log.head())
-    while True:
-        pass
-    """
-    preprocessor.import_event_log_csv("../data/running-example.csv"
-                                      , "case_id", "activity", "timestamp", ';')
-    """
-    preprocessor.import_event_log_csv("../data/running-example.csv"
-                                      , "case_id", "activity", "timestamp", ';')
-
-    
-    train, test, no_classes = preprocessor.split_train_test(.7)
-
-    nn_manager = NNManagement()
-    nn_manager.train(train, test, preprocessor.case_id_key, preprocessor.case_timestamp_key, preprocessor.case_activity_key, no_classes)
-    stats_in_json = nn_manager.get_training_statistics()
+    is_xes = False
+    if is_xes:
+        preprocessor.import_event_log_xes("../data/Hospital_log.xes")
+        print(preprocessor.event_log.head())
+    else:
+        preprocessor.import_event_log_csv("../data/train_day.csv"
+                                        , "case_id", "activity", "timestamp", ',')
+        train, test, no_classes = preprocessor.split_train_test(.9)
+        nn_manager = NNManagement()
+        nn_manager.train(train, test, preprocessor.case_id_key, preprocessor.case_timestamp_key, preprocessor.case_activity_key, no_classes)
+        stats_in_json = nn_manager.get_training_statistics()
     #nn_manager.model.predict_get_sorted(pass)
