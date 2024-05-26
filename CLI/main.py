56cda6f88a9d5c774931f30093d8a12b74ce192a
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2024-05-26 21:25:29 +0200

    generate predicitve log now as a route

--- CLI/main.py
+++ CLI/main.py
@@ -38,6 +38,40 @@ def test_train():
 
 
 
+def test_predictive_model_generator(
+        config,
+        path_to_model, 
+        path_to_log, 
+        non_stop, 
+        upper, 
+        random_cuts, 
+        cut_length, 
+        new_log_path
+    ):
+    params = {
+        "is_xes": False, 
+        "path_to_log": path_to_log, 
+        "sep": ",", 
+        "case_id": "case_id", 
+        "activity_key": "activity", 
+        "timestamp_key": "timestamp", 
+        "cuda": True, 
+        "path_to_model": path_to_model, 
+        "config": json.dumps(config), 
+        "non_stop": non_stop, 
+        "upper": upper, 
+        "random_cuts": random_cuts, 
+        "cut_length": cut_length, 
+        "new_log_path": new_log_path
+    }
+
+
+    response = requests.get(
+        "http://localhost:5000/generate_predictive_log", 
+        params = params,
+        timeout =6000
+    )
+    return json.loads(response.text)
 
 def test_multiple_prediction(config, path_to_model, path_to_log, depth, degree):
     params = {
@@ -90,4 +124,14 @@ def test_single_prediction(config, path_to_model, path_to_log):
 if __name__=="__main__":
     statistics, config = test_train()
     #test_single_prediction(config, "CLI/models/my_cool_model.pt", "CLI/input_logs/dummy.csv")
-    print(test_multiple_prediction(config, "CLI/models/my_cool_model.pt", "CLI/input_logs/dummy.csv", 2,2))
+    #print(test_multiple_prediction(config, "CLI/models/my_cool_model.pt", "CLI/input_logs/dummy.csv", 2,2))
+    test_predictive_model_generator(
+        config, 
+        "CLI/models/my_cool_mode.pt", 
+        "data/train_day_joined.csv", 
+        True, 
+        30, 
+        True, 
+        3, 
+        "CLI/predictive_logs/my_cool_predictive_log.csv"
+    )
