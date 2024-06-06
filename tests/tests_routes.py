736e355cc856db9afe821082d1220db8d1e8259c
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2024-06-06 16:54:59 +0200

    done preprocessing route fully tested

--- tests/tests_routes.py
+++ tests/tests_routes.py
@@ -8,9 +8,6 @@ import pandas as pd
 #: this module intends to tests the server routes
 
 
-SERVER_NAME="localhost"
-SERVER_PORT=8080
-
 
 class TestPreprocessingRoutes(unittest.TestCase):
     """
@@ -18,16 +15,19 @@ class TestPreprocessingRoutes(unittest.TestCase):
     """
     @classmethod
     def setUpClass(cls):
+        SERVER_NAME="localhost"
+        SERVER_PORT=8080
+
         cls.routes = [
-            "http://{SERVER_NAME}:{SERVER_PORT}/remove_duplicates",
-            "http://{SERVER_NAME}:{SERVER_PORT}/add_unique_start_end",
-            "http://{SERVER_NAME}:{SERVER_PORT}/replace_with_mode"
+            f"http://{SERVER_NAME}:{SERVER_PORT}/remove_duplicates",
+            f"http://{SERVER_NAME}:{SERVER_PORT}/add_unique_start_end",
+            f"http://{SERVER_NAME}:{SERVER_PORT}/replace_with_mode"
                       ]
         cls.dummy_params = {
             "path_to_log": "tests/dummy_data/Hospital_log_mini.xes" , 
             "case_id": "case:concept:name", 
             "activity_key":  "concept:name", 
-            "timestamp_key": "timestamp:key" , 
+            "timestamp_key": "time:timestamp" , 
             "is_xes": True, 
             "save_path": "tests/dummy_data/Hospital_log_mini_" ,
             "sep": ","
@@ -48,7 +48,7 @@ class TestPreprocessingRoutes(unittest.TestCase):
             data = response.json()
             self.assertDictEqual({"error": "is_xes should be boolean"}, data)
             response = requests.post(
-                self.route,
+                route,
                 json= {
                     "is_xes": "True"
                 },
@@ -71,7 +71,7 @@ class TestPreprocessingRoutes(unittest.TestCase):
             cpy = self.dummy_params.copy()
             cpy["path_to_log"] = "definitely not a path"
             response = requests.post(
-                f"http://{SERVER_NAME}:{SERVER_PORT}/remove_duplicates", 
+                self.routes[0],
                 json= cpy
             )
             self.assertEqual(400, response.status_code)
@@ -100,8 +100,8 @@ class TestPreprocessingRoutes(unittest.TestCase):
             self.assertEqual("error while importing",data["description"] )
 
     def test_no_duplicates(self): 
-        cpy = self.dummy_params.copy
-        new_path  = self.dummy_params['save_path']+self.routes[0]+".csv"
+        cpy = self.dummy_params.copy()
+        new_path  = self.dummy_params['save_path']+"dup"+".csv"
 
         cpy['save_path']= new_path
         response = requests.post(
@@ -111,25 +111,44 @@ class TestPreprocessingRoutes(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         data = response.json()
         self.assertEqual(new_path, data['save_path'])
-        file_exists = os.path.isfile(self.dummy_params['save_path'])
+        file_exists = os.path.isfile(new_path)
         self.assertTrue(file_exists)
         res = pd.read_csv(new_path, sep = ',')
         len_res = len(res)
         no_dup = res.drop_duplicates()
         self.assertEqual(len_res, len(no_dup))
-        os.remove(self.dummy_params["save_path"])
         
 
     def test_unique_start_end(self): 
+        cpy = self.dummy_params.copy()
+        new_path  = self.dummy_params['save_path']+"unique"+".csv"
+        cpy['save_path']= new_path
         response = requests.post(
             self.routes[1],
-            json= self.dummy_params
+            json= cpy
         )
+        data = response.json()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(new_path, data['save_path'])
+        file_exists = os.path.isfile(new_path)
+        self.assertTrue(file_exists)
+        df = pd.read_csv(new_path, sep = ',')
+        case_ids = df["case:concept:name"].unique()
+        check = True
+        start= df[df["case:concept:name"]==case_ids[0]].iloc[0]["concept:name"]
+        end = df[df["case:concept:name"]==case_ids[0]].iloc[-1]["concept:name"]
+        for case_id in case_ids: 
+            sub_df = df[df["case:concept:name"]==case_id]
+            sub_df = sub_df["concept:name"]
+            if sub_df.iloc[0] != start or sub_df.iloc[-1] != end: 
+                check = False
+                break
+        self.assertTrue(check)
 
 
     def test_replace_with_mode(self): 
-        cpy = self.dummy_params.copy
-        new_path  = self.dummy_params['save_path']+self.routes[2]+".csv"
+        cpy = self.dummy_params.copy()
+        new_path  = self.dummy_params['save_path']+"mode"+".csv"
 
         cpy['save_path']= new_path
         response = requests.post(
@@ -138,10 +157,11 @@ class TestPreprocessingRoutes(unittest.TestCase):
         )
         self.assertEqual(200, response.status_code)
         data = response.json()
-        self.assertEqual(self.dummy_params['save_path'], data['save_path'])
-        file_exists = os.path.isfile(self.dummy_params['save_path'])
+        self.assertEqual(new_path, data['save_path'])
+        file_exists = os.path.isfile(new_path)
         self.assertTrue(file_exists)
-        os.remove(self.dummy_params["save_path"])
+        df = pd.read_csv(new_path, sep = ',')
+        self.assertEqual(0, df.isna().sum().sum() )
 
 
     @classmethod
@@ -150,20 +170,19 @@ class TestPreprocessingRoutes(unittest.TestCase):
         ensure to delete the generated csv's,as they will not be used any more. 
         """
         try:
-            os.remove(cls.dummy_params["save_path"]+cls.routes[2]+".csv")
-        except: 
-            pass
-            
-        try:
-            os.remove(cls.dummy_params["save_path"]+cls.routes[1]+".csv")
+            os.remove(cls.dummy_params["save_path"]+"mode"+".csv")
         except: 
             pass
 
         try:
-            os.remove(cls.dummy_params["save_path"]+cls.routes[0]+".csv")
+            os.remove(cls.dummy_params["save_path"]+"unique"+".csv")
         except: 
             pass
 
+        try:
+            os.remove(cls.dummy_params["save_path"]+"dup"+".csv")
+        except: 
+            pass
 
 if __name__ == "__main__":
     unittest.main()
