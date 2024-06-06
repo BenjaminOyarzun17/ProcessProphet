ff055c61d5c877be3bb69896c3c4d10b7da1f8b2
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2024-06-06 23:32:49 +0200

    ok testing train, random search, grid search routes done. error handling and exception handling in train routes also done

--- tests/tests_routes.py
+++ tests/tests_routes.py
@@ -5,14 +5,313 @@ import requests
 import pandas as pd
 
 
-#: this module intends to tests the server routes
+#: this module tests the server routes
 
 
 
+class TestTrainingRoutes(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        SERVER_NAME="localhost"
+        SERVER_PORT=8080
+
+        cls.routes = [
+            f"http://{SERVER_NAME}:{SERVER_PORT}/train_nn",
+            f"http://{SERVER_NAME}:{SERVER_PORT}/random_search",
+            f"http://{SERVER_NAME}:{SERVER_PORT}/grid_search" 
+                      ]
+        cls.dummy_params = {
+            "path_to_log": "tests/dummy_data/Hospital_log_mini.xes" , 
+            "case_id": "case:concept:name", 
+            "activity_key":  "concept:name", 
+            "timestamp_key": "time:timestamp" , 
+            "is_xes": True, 
+            "sep": ",", 
+            "cuda": True, 
+            "model_path": "tests/dummy_data/dummy_output_model.pt", 
+            "split": 0.5, 
+            "seq_len":10, 
+            "emb_dim":1000, 
+            "hid_dim":1000, 
+            "mlp_dim":1000, 
+            "lr":0.001, 
+            "batch_size":200, 
+            "epochs": 2, 
+            "search_params": {
+                "hid_dim": [100, 200],
+                "emb_dim":[100, 200],
+                "mlp_dim":[100, 200]
+            },
+            "iterations": 1
+        }
+        try:
+            os.remove("tests/dummy_data/dummy_output_model.pt")
+        except: 
+            pass
+        try:
+            os.remove("tests/dummy_data/dummy_output_model.config.json")
+        except: 
+            pass
+
+        try:
+            os.remove("tests/dummy_data/some_other_new_model.pt")
+        except: 
+            pass
+        try:
+            os.remove("tests/dummy_data/some_other_new_model.config.json")
+        except: 
+            pass
+
+    def test_wrong_booleans(self):
+        for route in self.routes:
+            cpy = self.dummy_params.copy()
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"] = [100, 200, 100]
+                cpy["search_params"]["mlp_dim"]  = [100, 200, 100]
+                cpy["search_params"]["hid_dim"] = [100, 200, 100]
+            for param in ["cuda", "is_xes"]:
+                cpy[param]= "definitely not a boolean"
+                response = requests.post(
+                    route,
+                    json= cpy, 
+                    timeout =6000
+                )
+                self.assertEqual(400, response.status_code)
+                data = response.json()
+                self.assertEqual(data["error"], "a boolean param was set to another type")
+            
+    def test_wrong_integer_params(self): 
+        for route in self.routes:
+            cpy = self.dummy_params.copy()
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            if "train" in route:
+                integer_params = ["seq_len", "emb_dim", "hid_dim", "mlp_dim", "batch_size", "epochs"]
+            elif "random" in route:
+                integer_params = ["seq_len",  "batch_size", "epochs", "iterations"]
+            elif "grid" in route:
+                integer_params = ["seq_len",  "batch_size", "epochs"]
+            for param in integer_params: 
+                cpy[param]= "definitely not an int value"
+                response = requests.post(
+                    route,
+                    json= cpy, 
+                    timeout =6000
+                )
+                self.assertEqual(400, response.status_code)
+                data = response.json()
+                self.assertEqual(data["error"], "an integer param was set to another type")
+    
+
+    def test_search_params(self): 
+        for route in self.routes[1:]:
+            cpy = self.dummy_params.copy()
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            cpy["search_params"]= {
+                "emb_dim": [],
+                "hid_dim":[]
+            }
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "missing key in search params")
+
+
+            cpy = self.dummy_params.copy()
+            cpy["search_params"]= {
+                "emb_dim": [],
+                "hid_dim":[], 
+                "mlp_dim":[]
+            }
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "search param(s) missing")
+
+
+            cpy = self.dummy_params.copy()
+            if "grid" in route:
+                cpy["search_params"]= {
+                    "emb_dim": [100, 200, "watermelon"],
+                    "hid_dim":[100, 200, 100], 
+                    "mlp_dim":[100,200,100]
+                }
+
+            elif "random" in route: 
+                cpy["search_params"]= {
+                    "emb_dim": [100, "rhabababa"],
+                    "hid_dim":[100, 200], 
+                    "mlp_dim":[100,200]
+                }
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "non integer found in search params")
+
+
+
+
+
+
+    def test_wrong_float_params(self): 
+        for route in self.routes:
+            cpy = self.dummy_params.copy()
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            float_params = ["lr", "split"]
+            for param in float_params: 
+                cpy[param]= "definitely not a float value"
+                response = requests.post(
+                    route,
+                    json= cpy, 
+                    timeout =6000
+                )
+                self.assertEqual(400, response.status_code)
+                data = response.json()
+                self.assertEqual(data["error"], "a float param was set to another type")
+
+
+    def test_paths(self): 
+        for route in self.routes:
+            cpy = self.dummy_params.copy()
+            cpy["path_to_log"]= "definitely not a path in any OS"
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "one required path does not exist")
+
+
+        for route in self.routes:
+            cpy = self.dummy_params.copy()
+            cpy["model_path"]= "tests/dummy_data/DONOTDELETE.pt" 
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "the target path for the new file already exists")
+         
+    def test_split_train_error(self):
+        for route in self.routes: 
+            cpy = self.dummy_params.copy()
+            cpy["split"]= 777.777
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            elif "random" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200]
+                cpy["search_params"]["mlp_dim"]=[100,200]
+                cpy["search_params"]["hid_dim"]=[100,200]
+
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["error"], "train percentage must be in range (0,1) and should not yield empty sublogs")
+
+
+    def test_wrong_column(self):
+        for route in self.routes: 
+            cpy = self.dummy_params.copy()
+            cpy["case_id"]= "clearly not a column"
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            elif "random" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200]
+                cpy["search_params"]["mlp_dim"]=[100,200]
+                cpy["search_params"]["hid_dim"]=[100,200]
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            self.assertEqual(400, response.status_code)
+            data = response.json()
+            self.assertEqual(data["description"], "error while importing")
+
+
+    def test_training_results(self): 
+        for route in self.routes: 
+            cpy = self.dummy_params.copy()
+            cpy["model_path"] = "tests/dummy_data/some_other_new_model.pt"
+            if "grid" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200,100]
+                cpy["search_params"]["mlp_dim"]=[100,200,100]
+                cpy["search_params"]["hid_dim"]=[100,200,100]
+            elif "random" in route: 
+                cpy["search_params"]["emb_dim"]=[100,200]
+                cpy["search_params"]["mlp_dim"]=[100,200]
+                cpy["search_params"]["hid_dim"]=[100,200]
+            response = requests.post(
+                route,
+                json= cpy, 
+                timeout =6000
+            )
+            data = response.json()
+            print(data)
+            self.assertEqual(200, response.status_code, cpy)
+
+            model_exists= os.path.isfile(cpy["model_path"])
+            config_exists= os.path.isfile(cpy["model_path"][:-3]+".config.json")
+            self.assertTrue(model_exists and config_exists)
+            self.assertTrue("training_statistics" in data.keys() or "acc" in data.keys())
+            os.remove("tests/dummy_data/some_other_new_model.pt")
+            os.remove("tests/dummy_data/some_other_new_model.config.json")
+
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        try:
+            os.remove("tests/dummy_data/some_other_new_model.pt")
+        except: 
+            pass
+        try:
+            os.remove("tests/dummy_data/some_other_new_model.config.json")
+        except: 
+            pass
+
+"""
 class TestPreprocessingRoutes(unittest.TestCase):
-    """
-    tests the three preprocessing routes
-    """
+    #:tests the three preprocessing routes
     @classmethod
     def setUpClass(cls):
         SERVER_NAME="localhost"
@@ -166,9 +465,7 @@ class TestPreprocessingRoutes(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        """
-        ensure to delete the generated csv's,as they will not be used any more. 
-        """
+        #:ensure to delete the generated csv's,as they will not be used any more. 
         try:
             os.remove(cls.dummy_params["save_path"]+"mode"+".csv")
         except: 
@@ -183,6 +480,10 @@ class TestPreprocessingRoutes(unittest.TestCase):
             os.remove(cls.dummy_params["save_path"]+"dup"+".csv")
         except: 
             pass
+
+"""
+
+
 
 if __name__ == "__main__":
     unittest.main()
