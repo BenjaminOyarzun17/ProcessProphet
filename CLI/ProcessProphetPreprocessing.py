2de8c0dfdb3417b8367b42d2e5beca12b54965fc
maxi-pjk <97049762+maxi-pjk@users.noreply.github.com>
2024-06-11 15:04:18 +0200

    ProcessProphetPreprocessing docs

--- CLI/ProcessProphetPreprocessing.py
+++ CLI/ProcessProphetPreprocessing.py
@@ -15,6 +15,11 @@ TIMEOUT= os.getenv('TIMEOUT')
 
 class ProcessProphetPreprocessing: 
     def __init__(self, pp):
+        """
+        initialize ProcessProphet Object and Preprocessing main menu
+
+        :param pp: the ProcessProphet instance in charge of window management 
+        """
         self.pp = pp #: reference to the ProcessProphet object
         self.pp.switch_window(self.preprocessing_main_menu()) #: starts with the preprocessing main menu
 
@@ -23,8 +28,30 @@ class ProcessProphetPreprocessing:
     #: this decorator is used for type checking
     @staticmethod
     def check_types(func):
+        """
+        decorator that can be called to check if the file is an
+        excepted file type (xes/csv) and the file also exists in
+        the directory of the project
+
+        :func: the function that check_types decorates
+
+        side effect:
+        if the restrictions are not followed a new window with the
+        corresponding error is indicated
+        """
         def wrapper(self, *args, **kwargs):
-            if self.log_name.value[-3:]!="xes" and self.log_name.value[-3:] != "csv": 
+            """
+            first it checks if the file is an excepted file type 
+            (xes/csv) and the file also exists in the directory of 
+            the project and then calls the original function
+
+            :*args:, :**kwargs: parameters to store an unspecified 
+            number auf arguments and keyword arguments because we dont
+            necessarily know how many arguments the function we call the
+            decorator on needs
+            """
+            if self.log_name.value[-3:]!="xes" and self.log_name.value[-3:] != "csv":
+                # error if file is in the wrong file type 
                 container= ptg.Container( 
                     ptg.Label(f"only xes/csv supported"),
                     ptg.Button("[black]back", lambda *_: self.pp.switch_window(self.preprocessing_main_menu()))
@@ -33,6 +60,7 @@ class ProcessProphetPreprocessing:
                 window.center()
                 return window
             elif self.log_name.value not in os.listdir(self.pp.state.input_logs_path):
+                # error if file does not exist in the directory
                 container= ptg.Container( 
                     ptg.Label(f"the log does not exist"),
                     ptg.Button("[black]back", lambda *_: self.pp.switch_window(self.preprocessing_main_menu()))
@@ -40,11 +68,15 @@ class ProcessProphetPreprocessing:
                 window = ptg.Window( container, box="DOUBLE")
                 window.center()
                 return window
-
+            # call original function the decorator was called on
             return func(self,*args, **kwargs)
         return wrapper
    
-    def loading(self, message = ""): 
+    def loading(self, message = ""):
+        """
+        function to indicate a message in a new window e.g. to 
+        show that a process is loading
+        """ 
         container = ptg.Container(
             "Loading...", 
             message
@@ -60,25 +92,19 @@ class ProcessProphetPreprocessing:
 
 
 
-    def loading(self, message = ""): 
-        """
-        a loading screen 
-        """
-        container = ptg.Container(
-            "Loading...", 
-            message
-        )
-        window = ptg.Window(*container, box="DOUBLE")
-        window.center()
-        self.pp.switch_window(window)
-
-
     @check_types
     def handle_replace_nan_with_mode(self):
         """
-        makes request to the server to replace nan values with mode in the activity column. 
-        """
-        self.loading("preprocessing data...")
+        first decorator is used to ensure the file can be preprocessed
+
+        sends a request to the server with all the needed parameters to do replace all NaN values in the log
+        and in case of a successful computation of the request by the server the path where the preprocessed log
+        is stored in will be indicated in a new window
+
+        if the request fails because e.g. it exceeds the timeout of TIMEOUT the error is displayed in a new window and 
+        the user can go back to the window where the parameters are displayed
+        """ 
+        self.loading("preprocessing data...") # loading screen while data is being preprocessed
         input_logs_path= self.pp.state.input_logs_path
         
         #: checks if extension is xes. otherwise csv assumed
@@ -100,6 +126,7 @@ class ProcessProphetPreprocessing:
             timeout =TIMEOUT
         )
         if response.status_code==200:
+            # succesful and indicate path for preprocessed data
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"success"),
@@ -107,7 +134,8 @@ class ProcessProphetPreprocessing:
                 "",
                 ptg.Button(f"{self.pp.button_color}back", lambda *_: self.pp.switch_window(self.preprocessing_main_menu())),
             )
-        else: 
+        else:
+            # error ocurred 
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"error: {data['error']}"),
@@ -121,8 +149,17 @@ class ProcessProphetPreprocessing:
 
     @check_types
     def handle_remove_duplicates(self):
+        """
+        first decorator is used to ensure the file can be preprocessed
 
-        self.loading("preprocessing data...")
+        sends a request to the server with all the needed parameters to do remove duplicate rows from the log
+        and in case of a successful computation of the request by the server the path where the preprocessed log
+        is stored in will be indicated in a new window
+
+        if the request fails because e.g. it exceeds the timeout of TIMEOUT the error is displayed in a new window and 
+        the user can go back to the window where the parameters are displayed
+        """
+        self.loading("preprocessing data...") # loading screen while data is being preprocessed
         input_logs_path= self.pp.state.input_logs_path
         
         is_xes = True if self.log_name.value[-3:] == "xes"  else False
@@ -143,6 +180,7 @@ class ProcessProphetPreprocessing:
             timeout =TIMEOUT
         )
         if response.status_code==200:
+            # successful and indicate path for preprocessed data
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"success"),
@@ -150,7 +188,8 @@ class ProcessProphetPreprocessing:
                 "",
                 ptg.Button(f"{self.pp.button_color}back", lambda *_: self.pp.switch_window(self.preprocessing_main_menu())),
             )
-        else: 
+        else:
+            # error ocurred 
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"error: {data['error']}"),
@@ -168,8 +207,17 @@ class ProcessProphetPreprocessing:
 
     @check_types
     def handle_add_unique_start_end(self):
+        """
+        first decorator is used to ensure the file can be preprocessed
 
-        self.loading("preprocessing data...")
+        sends a request to the server with all the needed parameters to do add unique start end end activities to each trace
+        and in case of a successful computation of the request by the server the path where the preprocessed log
+        is stored in will be indicated in a new window
+
+        if the request fails because e.g. it exceeds the timeout of TIMEOUT the error is displayed in a new window and 
+        the user can go back to the window where the parameters are displayed
+        """
+        self.loading("preprocessing data...") # loading screen while data is being preprocessed
         input_logs_path= self.pp.state.input_logs_path
         is_xes = True if self.log_name.value[-3:] == "xes"  else False
         
@@ -189,6 +237,7 @@ class ProcessProphetPreprocessing:
             timeout =TIMEOUT
         )
         if response.status_code==200:
+            # successful request
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"success"),
@@ -197,6 +246,7 @@ class ProcessProphetPreprocessing:
                 ptg.Button(f"{self.pp.button_color}back", lambda *_: self.pp.switch_window(self.preprocessing_main_menu())),
             )
         else: 
+            # an error occured
             data = response.json()
             container= ptg.Container( 
                 ptg.Label(f"error: {data['error']}"),
@@ -209,12 +259,25 @@ class ProcessProphetPreprocessing:
         return window
 
 
-    def add_unique_start_end(self): 
+    def add_unique_start_end(self):
+        """
+        This function indicates all the parameters that are needed to add unique start
+        and end activities to each trace and the user can modify them in the left side of the window
+
+        The function also indicates the first few Log file names in the current project on
+        the right side of the window
+
+        Side effects:
+        -initializes window with default parameters where the user can adjust them
+        -initializes window where all the event logs of the current project are listed that can be used
+        for the preprocessing
+        -add_unique_start_end can be called if the user confirms the indicated parameters
+        """ 
         self.log_name=  ptg.InputField("Hospital_log.xes", prompt="log name: ")
         self.case_id_key=  ptg.InputField("case:concept:name", prompt="case id key: ")
         self.case_activity_key=  ptg.InputField("concept:name", prompt="activity key: ")
         self.case_timestamp_key=  ptg.InputField("time:timestamp", prompt="timestamp key: ")
-        self.save_path=  ptg.InputField("HL_unique_start.csv", prompt="output log name:")
+        self.save_path=  ptg.InputField("HL_unique_start.csv", prompt="output log name:") # Name of the preprocessed copy of the log
 
         left_container = ptg.Container( 
             ptg.Label(f"enter relevant information"),
@@ -244,12 +307,25 @@ class ProcessProphetPreprocessing:
 
 
 
-    def remove_duplicates(self): 
+    def remove_duplicates(self):
+        """
+        This function indicates all the parameters that are needed to remove duplicate rows
+        and the user can modify them in the left side of the window
+
+        The function also indicates the first few Log file names in the current project on
+        the right side of the window
+
+        Side effects:
+        -initializes window with default parameters where the user can adjust them
+        -initializes window where all the event logs of the current project are listed that can be used
+        for the preprocessing
+        -remove_duplicates can be called if the user confirms the indicated parameters
+        """ 
         self.log_name=  ptg.InputField("Hospital_log.xes", prompt="log name: ")
         self.case_id_key=  ptg.InputField("case:concept:name", prompt="case id key: ")
         self.case_activity_key=  ptg.InputField("concept:name", prompt="activity key: ")
         self.case_timestamp_key=  ptg.InputField("time:timestamp", prompt="timestamp key: ")
-        self.save_path=  ptg.InputField("HL_no_dup.csv", prompt="output log name:")
+        self.save_path=  ptg.InputField("HL_no_dup.csv", prompt="output log name:") # Name of the preprocessed copy of the log
 
         left_container = ptg.Container( 
             ptg.Label(f"enter relevant information"),
@@ -278,13 +354,26 @@ class ProcessProphetPreprocessing:
 
 
 
-    def replace_nan_with_mode(self): 
+    def replace_nan_with_mode(self):
+        """
+        This function indicates all the parameters that are needed to replace NaN values
+        and the user can modify them in the left side of the window
+
+        The function also indicates the first few Log file names in the current project on
+        the right side of the window
+
+        Side effects:
+        -initializes window with default parameters where the user can adjust them
+        -initializes window where all the event logs of the current project are listed that can be used
+        for the preprocessing
+        -replace_nan_with_mode can be called if the user confirms the indicated parameters
+        """ 
         self.log_name=  ptg.InputField("Hospital_log.xes", prompt="log name: ")
         self.case_id_key=  ptg.InputField("case:concept:name", prompt="case id key: ")
         self.case_activity_key=  ptg.InputField("concept:name", prompt="activity key: ")
         self.case_timestamp_key=  ptg.InputField("time:timestamp", prompt="timestamp key: ")
-        self.save_path=  ptg.InputField("HL_nan_to_mode.csv", prompt="output log name:")
-
+        self.save_path=  ptg.InputField("HL_nan_to_mode.csv", prompt="output log name:") # Name of the preprocessed copy of the log
+        #indicates params
         left_container = ptg.Container( 
             ptg.Label(f"enter relevant information"),
             self.log_name,
@@ -300,20 +389,30 @@ class ProcessProphetPreprocessing:
         
         logs = [log for log in os.listdir(self.pp.state.input_logs_path)]
         logs = logs[:min(len(logs),4 )] #: to not overflow the terminal
-
+        #shows logs in the current project
         right_container = ptg.Container(
             f"[underline]First {len(logs)} logs in project:", *logs
         ).center()
 
         window = ptg.Window(ptg.Splitter(left_container, right_container), width = self.pp.window_width)
-        #window = ptg.Window(*container)
         window.center()
         return window
 
 
 
 
-    def preprocessing_main_menu(self) : 
+    def preprocessing_main_menu(self):
+        """
+        this function displays the main menu for the preprocessing manager. 
+
+        depending on how the data should be preprocessed the user can choose
+        one of the three alternatives
+            1. replacing NaN values in the log
+            2. removing duplicate rows in the log
+            3. adding unique start and end activities to each trace
+
+        it is also possible to return to the previous menu.
+        """
         replace = "replace NaN in activity column with mode"
         remove= "remove duplicate rows"
         add= "add unique start and end activities"
